4d6efd78ec49d8a0b51a50ae122373990784522a
jean-valentin <jvalentin.developer@gmail.com>
2020-12-02 18:02:35 -0300

    atualização do index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,5 @@
-import { useState } from 'react';
-
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-            <br></br>
-            <h3>versão 1.0.0 beta</h3>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-        return (
-            <div>
-                <div>{contador}</div>
-                <button onClick={adicionarContador}>Adicionar</button>
-            </div>
-        )
+    return <h1>Home</h1>          
 }
 
 export default Home
